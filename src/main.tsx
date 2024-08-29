88813f325a685f9c7a2cdb93a6a99d95a9eefb5c
NoorBloach <noorbloach1@gmail.com>
2024-08-29 14:18:14 +0500

    commented

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <Provider store={store}>
       <Router />
     </Provider>
-    <ScrollToTop />
+    <ScrollToTop /> 
   </BrowserRouter>
 );
