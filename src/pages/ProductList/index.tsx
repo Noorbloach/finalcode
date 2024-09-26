4ff1e0b1b6d4ef579d1a6357c99a9ae8f1b311ce
Azaan <azaanoor18@gmail.com>
2024-09-26 23:44:16 +0500

    feat:"joined member and estimator"

--- src/pages/ProductList/index.tsx
+++ src/pages/ProductList/index.tsx
@@ -356,9 +356,7 @@ const ensureProtocol = (url: string) => {
         {(role === "admin" || role === "employee") && (
         <Table.Th className="text-center border-b-0 whitespace-nowrap">Joined Members</Table.Th>)}
         <Table.Th className="text-center border-b-0 whitespace-nowrap">Project (admin) Link</Table.Th>
-        {(role === "admin" || role === "employee" ) && (
-        <Table.Th className="text-center border-b-0 whitespace-nowrap">Estimator Link</Table.Th>
-      )}
+       
       {/* Conditionally render Template column */}
       {(role === "admin" || role === "employee" ) && (
         <Table.Th className="text-center border-b-0 whitespace-nowrap">Template</Table.Th>
@@ -403,18 +401,7 @@ const ensureProtocol = (url: string) => {
     Admin Link
   </a>
 </Table.Td>
-{(role === "admin" || role === "employee") && (
-  <Table.Td className="text-center">
-    <a 
-      href={ensureProtocol(project.estimatorLink)} 
-      className="underline text-blue-500 hover:text-blue-700" 
-      target="_blank" 
-      rel="noopener noreferrer"
-    >
-      Estimator Link
-    </a>
-  </Table.Td>
-)}
+
 
         {/* Conditionally render Template field */}
         {(role === "admin" || role === "employee") && (
