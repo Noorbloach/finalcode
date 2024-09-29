7623575b9df5ac2bd590267d60830aea8795c620
Azaan <azaanoor18@gmail.com>
2024-09-30 00:20:10 +0500

    feat:"management"

--- src/pages/ProjectApproved/index.tsx
+++ src/pages/ProjectApproved/index.tsx
@@ -271,7 +271,7 @@ function Main() {
       <h2 className="mt-10 text-lg font-medium intro-y">Project List</h2>
       <div className="grid grid-cols-12 gap-6 mt-5">
         <div className="flex flex-wrap items-center col-span-12 mt-2 intro-y sm:flex-nowrap">
-        {(role === "superadmin") && (
+        {(role === "superadmin" || role === "management") && (
           <Button variant="primary" className="mr-2 shadow-md" onClick={() => navigate('/add-product')}>
             Add New Project
           </Button>)}
